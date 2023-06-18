a3b9226039ad495821eaaf088437cbc524d9e7cb
TheCustomFHD <thecustomfhd@gmail.com>
2023-06-18 21:12:44 +0200

    Add ErrorCheck to fwrite(), unsure if effective

--- src/main.c
+++ src/main.c
@@ -4,15 +4,32 @@
 
 int main(void)
 {
+    //Reusable Variables
+    int functionOutput = 0;
+
     int width = 2;
-    int height = 2;
+    int height = 3;
     int colordepth = 1;
 
     //Array must be (Width * Height * 3). 3 for RGB, and must be a uint8_t, to stay under 8Bit (255 MAX color value for PPM)
-    uint8_t samplePic[12] = {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1};
+    uint8_t samplePic[18] = {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1};
 
-    printf("Starting Writing PPM\n");
-    writeToPPM("out.ppm", width, height, colordepth, samplePic);
+    printf("[Info] Starting to write to PPM File (P6)\n");
+    functionOutput = writeToPPM("out.ppm", width, height, colordepth, samplePic);
+    switch(functionOutput)
+    {
+        case 1:
+            printf("[ERROR] File Error\n");
+            break;
+        
+        case 2:
+            printf("[ERROR] fwrite() Error\n");
+            break;
+
+        default:
+            printf("[Info] Writing Successful\n");
+            break;
+    }
 
     return 0;
 }
--- src/writer.c
+++ src/writer.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+//ToDo: Rename Function and "Libary", include that this function does P6 Binary
 int writeToPPM(char *path, int width, int height, int colorRange, uint8_t pixelsRGB[])
 {
+    //Reusable Variables
+    int functionOutput = 0;
+
     //Open FilePointer and Specify its overwrite and binary
     FILE *fptr;
     fptr = fopen(path, "wb");
     if (fptr == NULL)
     {
         perror("[ERROR] Status of fopen() in overwrite-text mode failed:");
-        exit(1);
+        return 1;
     }
 
     /*
@@ -22,14 +26,23 @@ int writeToPPM(char *path, int width, int height, int colorRange, uint8_t pixels
     char header[4] = "P6";
     fprintf(fptr, "%s\n%d %d\n%d\n", header, width, height, colorRange);
     
-    //Calculate Total Array Length // ToDo: Figure out why -1 is needed, assumption because of a terminator or 0 as array start
+    //Calculate Total Array Length
     int arrayLength = (width * height * 3);
 
     //Write Pixel Data
-    fwrite(pixelsRGB, sizeof(uint8_t), arrayLength, fptr); 
+    functionOutput = fwrite(pixelsRGB, sizeof(uint8_t), arrayLength, fptr); 
+    if (functionOutput != arrayLength)
+    {
+        //Error Checking, unsure of its effectivness, ImproveMe:
+        int functionOutput = ferror(fptr);
+        printf("[ERROR] fwrite() had an error, quitting: %d\n", functionOutput);
+        clearerr(fptr);
+        return 2;
+    }
+
+    //printf("[DEBUG] FWRITE OUTPUT: %d\n", functionOutput);
 
     //Close Filepointer(Cleanup) and return
     fclose(fptr);
     return 0;
 }
-//ToDo: Make it accept some sort of Input Array or something like that.
